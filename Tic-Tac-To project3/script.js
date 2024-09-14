4ab57ec2d044bdc37c1faf0147f98dd90c389ae6
Habeeba Khatoon <Khabeeba18@gmail.com>
2024-09-14 13:25:50 +0530

    step 2 completed

--- Tic-Tac-To project3/script.js
+++ Tic-Tac-To project3/script.js
@@ -1,1 +1,9 @@
 console.log("starting here!");
+
+function getComputerChoice(){
+    let choices = ["rock","paper","scissors"];
+    let index = Math.floor(Math.random()*3);
+    return choices[index];
+}
+
+console.log(getComputerChoice());
